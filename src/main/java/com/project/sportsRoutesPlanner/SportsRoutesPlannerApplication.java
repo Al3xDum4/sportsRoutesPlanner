9cab065a41df75408cb35f28f742f9692123c61c
Alex Duma <alex.m.duma@gmail.com>
2020-08-22 23:10:40 +0300

    first commit

---
+++ src/main/java/com/project/sportsRoutesPlanner/SportsRoutesPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.project.sportsRoutesPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsRoutesPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsRoutesPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/sportsRoutesPlanner/SportsRoutesPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sportsRoutesPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsRoutesPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
