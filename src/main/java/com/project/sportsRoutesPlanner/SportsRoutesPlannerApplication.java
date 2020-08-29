8e75f43e0b44c00e915fff8a8278164051d362b9
Alex Duma <alex.m.duma@gmail.com>
2020-08-29 14:03:53 +0300

    controllers, security and some templates

--- src/main/java/com/project/sportsRoutesPlanner/SportsRoutesPlannerApplication.java
+++ src/main/java/com/project/sportsRoutesPlanner/SportsRoutesPlannerApplication.java
@@ -1,13 +1,34 @@
 package com.project.sportsRoutesPlanner;
 
+import com.project.sportsRoutesPlanner.repository.*;
+import com.project.sportsRoutesPlanner.service.JavaMailSenderService;
+import org.apache.catalina.realm.X509UsernameRetriever;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SportsRoutesPlannerApplication {
+public class SportsRoutesPlannerApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SportsRoutesPlannerApplication.class, args);
-	}
+    @Autowired
+    private EventRepository eventRepository;
+    @Autowired
+    private RouteRepository routeRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PhotoRepository photoRepository;
+    @Autowired
+    private JavaMailSenderService javaMailSenderService;
+    @Autowired
+    private PendingUserRepository pendingUserRepository;
+
+	public static void main(String[] args) { SpringApplication.run(SportsRoutesPlannerApplication.class, args); }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 
 }
