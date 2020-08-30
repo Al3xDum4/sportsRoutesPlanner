be9c35240147a697e1faa6ea44d02522e0067757
Andreea Sperdea <andreea.sperdea@gmail.com>
2020-08-30 12:26:37 +0300

    UserRepository updated - findByUserRole

--- src/main/java/com/project/sportsRoutesPlanner/repository/UserRepository.java
+++ src/main/java/com/project/sportsRoutesPlanner/repository/UserRepository.java
@@ -1,10 +1,16 @@
 package com.project.sportsRoutesPlanner.repository;
 
+import com.project.sportsRoutesPlanner.model.Role;
 import com.project.sportsRoutesPlanner.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
+
+    @Query("FROM Users u where u.users.role_name = :role_name")
+    Optional<Role> findUserByRole(@Param("role_name") String rolename);
 }
