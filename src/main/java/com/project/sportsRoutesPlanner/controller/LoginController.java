3fe211885034d60b91be44c76ee3910ca8ed7429
Andreea Sperdea <andreea.sperdea@gmail.com>
2020-08-31 13:08:24 +0300

    different corrections

--- src/main/java/com/project/sportsRoutesPlanner/controller/LoginController.java
+++ src/main/java/com/project/sportsRoutesPlanner/controller/LoginController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
-    @Autowired
-    private UserService userService;
 
     @GetMapping("/login")
     public String loginPage() {
