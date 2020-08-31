1d2e862e33649c46c74ba6be7037fb6b06c52cb4
Andreea Sperdea <andreea.sperdea@gmail.com>
2020-08-31 12:41:23 +0300

    update in main

--- src/main/java/com/project/sportsRoutesPlanner/SportsRoutesPlannerApplication.java
+++ src/main/java/com/project/sportsRoutesPlanner/SportsRoutesPlannerApplication.java
@@ -1,12 +1,18 @@
 package com.project.sportsRoutesPlanner;
 
+import com.project.sportsRoutesPlanner.model.Role;
+import com.project.sportsRoutesPlanner.model.User;
 import com.project.sportsRoutesPlanner.repository.*;
 import com.project.sportsRoutesPlanner.service.JavaMailSenderService;
+import net.bytebuddy.implementation.bind.annotation.BindingPriority;
 import org.apache.catalina.realm.X509UsernameRetriever;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -26,11 +32,27 @@ public class SportsRoutesPlannerApplication implements CommandLineRunner, WebMvc
     @Autowired
     private PendingUserRepository pendingUserRepository;
 
+    @Bean
+    private PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
 	public static void main(String[] args) { SpringApplication.run(SportsRoutesPlannerApplication.class, args); }
 
     @Override
     public void run(String... args) throws Exception {
-
+	   User user = new User();
+	   user.setFirstName("Andreea");
+	   user.setLastName("Sperdea");
+	   user.setUsername("andreea");
+	   user.setPassword(encoder().encode("1234"));
+	   user.setAge(35);
+	   user.setEmailAddress("a@b.com");
+	   user.setPhoneNo("0733");
+	   user.setRole(Role.ADMIN);
+	   userRepository.save(user);
+	   pendingUserRepository.deleteById(1);
     }
 
     @Override
