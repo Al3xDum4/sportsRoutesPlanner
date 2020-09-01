a95dcfe62e84581ff3818935d0fd5206e53cd4d6
Andreea Sperdea <andreea.sperdea@gmail.com>
2020-09-01 14:04:53 +0300

    GuideController updates

--- src/main/java/com/project/sportsRoutesPlanner/repository/UserRepository.java
+++ src/main/java/com/project/sportsRoutesPlanner/repository/UserRepository.java
@@ -1,11 +1,14 @@
 package com.project.sportsRoutesPlanner.repository;
 
+import com.project.sportsRoutesPlanner.model.Role;
 import com.project.sportsRoutesPlanner.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByUsername(String username);
+   Optional<User> findByUsername(String username);
 
-}
+   }
