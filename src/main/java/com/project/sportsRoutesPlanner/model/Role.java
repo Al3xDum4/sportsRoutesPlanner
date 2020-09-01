e3c70820efa2c613957940533b21a93071c6b869
Andreea Sperdea <andreea.sperdea@gmail.com>
2020-09-01 14:03:18 +0300

    Role enum changed to show text

--- src/main/java/com/project/sportsRoutesPlanner/model/Role.java
+++ src/main/java/com/project/sportsRoutesPlanner/model/Role.java
@@ -1,18 +1,21 @@
 package com.project.sportsRoutesPlanner.model;
 
 import lombok.*;
-
 import javax.persistence.*;
 import java.util.*;
+
+
 
 public enum Role {
     MEMBER("member"),
     GUIDE("guide"),
     ADMIN("admin");
 
-    private String roleName;
-
     Role(String roleName) {
         this.roleName = roleName;
     }
+
+    private String roleName;
+
+
 }
