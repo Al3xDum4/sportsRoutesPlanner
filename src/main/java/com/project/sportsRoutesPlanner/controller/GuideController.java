3c358adb804d728c369cd506bb8a9c2b8432a978
Andreea Sperdea <andreea.sperdea@gmail.com>
2020-09-01 13:48:20 +0300

    GuideController updates

--- src/main/java/com/project/sportsRoutesPlanner/controller/GuideController.java
+++ src/main/java/com/project/sportsRoutesPlanner/controller/GuideController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,30 +22,51 @@ public class GuideController {
 
     @GetMapping("allguides")
     public String showAllGuides(Model model) {
-        List<User> guidesList = userService.findUserByRole();
+        List<User> guidesList = userService.findGuide();
         model.addAttribute("guides", guidesList);
         return "guide/showallguides";
     }
 
     @GetMapping("/addguide")
-    public String addGuide(Model model){
-        model.addAttribute("guide",new User());
+    public String addGuide(Model model) {
+        model.addAttribute("guide", new User());
         return "guide/addguide";
     }
 
     @PostMapping("/addguide")
-    public String addGuide(String firstName, String lastName,
-                           Integer age, String email, String phoneNo) {
+    public String addGuide(@ModelAttribute User user, String firstName, String lastName,
+                           Integer age, String emailAddress, String phoneNo) {
 
-        User user=new User();
         user.setFirstName(firstName);
         user.setLastName(lastName);
         user.setAge(age);
-        user.setEmailAddress(email);
+        user.setEmailAddress(emailAddress);
         user.setPhoneNo(phoneNo);
         user.setRole(Role.GUIDE);
         userService.save(user);
         return "redirect:/allguides";
     }
 
+    @GetMapping("/deleteguide/{id}")
+    public String deleteGuide(@PathVariable Integer id) {
+        userService.deleteById(id);
+        return "redirect:/allguides";
+    }
+
+    @GetMapping("/editguide/{id}")
+    public String editGuide(Model model, @PathVariable Integer id) {
+        User guide = userService.findById(id);
+
+        model.addAttribute("guide", guide);
+        return "guide/editguide";
+    }
+
+    @PostMapping("/editguide/{id}")
+    public String editGuide(@ModelAttribute User guide, @PathVariable Integer id) {
+        userService.save(guide);
+        return "redirect:/allguides";
+    }
 }
+
+
+
