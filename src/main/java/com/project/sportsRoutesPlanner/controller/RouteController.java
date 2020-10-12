8dfd317f2aaac00f2a6686a2bda0ef32e794c8e4
Alex Duma <alex.m.duma@gmail.com>
2020-10-12 13:31:29 +0300

    upload blob

--- src/main/java/com/project/sportsRoutesPlanner/controller/RouteController.java
+++ src/main/java/com/project/sportsRoutesPlanner/controller/RouteController.java
@@ -59,8 +59,8 @@ public class RouteController {
                                  Double maxAltitude, Double duration, String difficultyLevel,
                                  @RequestParam("routeBackground") MultipartFile multipartFile) throws IOException {
 
-        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
-        route.setBackgroundImg(fileName);
+        //String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+        route.setImage(multipartFile.getBytes());
 
         route.setRouteName(routeName);
         route.setDescription(description);
@@ -72,19 +72,19 @@ public class RouteController {
 
         Route savedRoute = routeService.save(route);
 
-        String uploadDir = "./route-background/" + savedRoute.getRouteId();
-        Path uploadPath = Paths.get(uploadDir);
-        if (!Files.exists(uploadPath)) {
-            Files.createDirectories(uploadPath);
-        }
-
-        try (InputStream inputStream = multipartFile.getInputStream()) {
-            Path filePath = uploadPath.resolve(fileName);
-            System.out.println(filePath.toFile().getAbsolutePath());
-            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            throw new IOException("Could not save uploaded file: " + fileName);
-        }
+//        String uploadDir = "./route-background/" + savedRoute.getRouteId();
+//        Path uploadPath = Paths.get(uploadDir);
+//        if (!Files.exists(uploadPath)) {
+//            Files.createDirectories(uploadPath);
+//        }
+//
+//        try (InputStream inputStream = multipartFile.getInputStream()) {
+//            Path filePath = uploadPath.resolve(fileName);
+//            System.out.println(filePath.toFile().getAbsolutePath());
+//            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+//        } catch (IOException e) {
+//            throw new IOException("Could not save uploaded file: " + fileName);
+//        }
 
         return "redirect:/allhikingroutes";
     }
