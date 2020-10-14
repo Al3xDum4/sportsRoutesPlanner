c35700f5f26ba2b4fb74308d9558c5352384eb70
Alex Duma <alex.m.duma@gmail.com>
2020-10-14 23:22:20 +0300

    slick-event15

--- src/main/java/com/project/sportsRoutesPlanner/controller/MainController.java
+++ src/main/java/com/project/sportsRoutesPlanner/controller/MainController.java
@@ -1,13 +1,24 @@
 package com.project.sportsRoutesPlanner.controller;
 
+import com.project.sportsRoutesPlanner.model.Event;
+import com.project.sportsRoutesPlanner.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
 
+    @Autowired
+    private EventService eventService;
+
     @GetMapping("/")
-    public String showHomePage() {
+    public String showHomePage(Model model) {
+        List<Event> eventList = eventService.findAll();
+        model.addAttribute("allevents", eventList);
         return "homepage/showhomepage";
     }
 }
