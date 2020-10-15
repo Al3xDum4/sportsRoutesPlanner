3f8f616b1fc45a810000ac8588e7f6014d05dc5a
Alex Duma <alex.m.duma@gmail.com>
2020-10-15 12:50:52 +0300

    fix carousel 17

--- src/main/java/com/project/sportsRoutesPlanner/controller/HomepageController.java
+++ src/main/java/com/project/sportsRoutesPlanner/controller/HomepageController.java
@@ -13,7 +13,6 @@ import org.springframework.ui.Model;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -42,11 +41,10 @@ public class HomepageController {
     }
 
     @GetMapping("showevent/{id}")
-    public ModelAndView showEvent(@PathVariable Integer id) {
+    public String showEvent(Model model, @PathVariable Integer id) {
         Event event = eventService.findById(id);
-        ModelAndView modelAndView = new ModelAndView("showhikingevent");
-        modelAndView.addObject(event);
-        return modelAndView;
+        model.addAttribute("event", event);
+        return "../../event/showhikingevent";
     }
 
     @GetMapping(value = "/slick/event/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
